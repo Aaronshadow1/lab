c792560d74d2bea40f3bc7f9bf582f20d8f558db
Shadow <aaronvarghese660@gmail.com>
2025-09-18 11:32:16 +0530

    Add files via upload

---
+++ python even or odd.py
@@ -0,0 +1,11 @@
+def is_even(number):
+    if number%2==0:
+        return True
+    else:
+        return False
+num=int(input("Enter a number"))
+if is_even(num):
+    print(num,"is even")
+else:
+        print(num,"is odd")
+        
